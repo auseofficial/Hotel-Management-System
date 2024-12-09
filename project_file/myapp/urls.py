41697c77f187bd67584749c67ea700fdaa8a1310
Akib Us Suny Eshan <eshan.r029@gmail.com>
2024-12-09 11:53:24 +0600

    Updated

--- project_file/myapp/urls.py
+++ project_file/myapp/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.restaurant_list, name='restaurant_list'),  # Updated to show the restaurant list
 ]
--- project_file/project_file/urls.py
+++ project_file/project_file/urls.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
+admin.site.site_header="Hotel Management System"
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Corrected this line
